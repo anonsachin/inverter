65733f5ae517a32045f3c2e18ebd479aba2f44c4
sachin <sachingw+xcode@protonmail.com>
2025-12-27 19:57:35 +0530

    fixed some issues

--- src/project.v
+++ src/project.v
@@ -11,6 +11,7 @@ module tt_um_sachin_inverter (
 //    input  wire       VAPWR,    // 3.3v power supply
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     inout  wire [7:0] ua,       // Analog pins, only ua[5:0] can be used
